108575431cc086cf59fc177fab19bf6c7d161b8a
Robin Lindner <robin@deeprobin.de>
2022-03-17 18:48:30 +0100

    Remove redundant using

--- src/coreclr/System.Private.CoreLib/src/System/Array.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Array.CoreCLR.cs
@@ -4,7 +4,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
